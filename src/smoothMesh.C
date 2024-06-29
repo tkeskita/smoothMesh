8ffd50a528c5ae4088ac9daca80cc53db567fead
Tuomo Keskitalo <tuomo.keskitalo@iki.fi>
2024-06-29 20:05:13 +0300

    First test version of smoothMesh
    - Skeleton for main program, which carries out centroidal smoothing
      of internal mesh points.
    - Reads in mesh, does one smoothing iteration, and saves mesh
      to a new time directory.

---
+++ src/smoothMesh.C
@@ -0,0 +1,180 @@
+/*---------------------------------------------------------------------------*\
+Application
+    smoothMesh
+
+Description
+    Smooth internal mesh points to improve mesh quality
+\*---------------------------------------------------------------------------*/
+
+#include "argList.H"
+#include "Time.H"
+#include "fvMesh.H"
+#include "volFields.H"
+#include "surfaceFields.H"
+#include "pointFields.H"
+#include "ReadFields.H"
+#include "regionProperties.H"
+#include "syncTools.H"
+#include "weightedPosition.H"
+
+using namespace Foam;
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+// Function for centroidal smoothing of internal mesh points.
+// Adapted from Foam::snappySnapDriver::smoothInternalDisplacement in
+// https://develop.openfoam.com/Development/openfoam/-/blob/OpenFOAM-v2312/src/mesh/snappyHexMesh/snappyHexMeshDriver/snappySnapDriver.C
+
+Foam::tmp<Foam::pointField> centroidalSmoothing
+(
+    const fvMesh& mesh
+)
+{
+    // Points for smoothing, starting from all points
+    bitSet isMovingPoint(mesh.nPoints(), true);
+
+    // Remove points on boundary patches
+    const faceList& faces = mesh.faces();
+    for (label faceI = mesh.nInternalFaces(); faceI < mesh.nFaces(); faceI++)
+    {
+	const face& f = mesh.faces()[faceI];
+	forAll (f, pointI)
+	{
+	    const label i = faces[faceI][pointI];
+	    isMovingPoint.unset(i);
+        }
+    }
+
+    // Centroidal smoothing algorithm
+
+    // Calculate number and sum of surrounding cell center
+    // coordinates using weightedPosition class for data storage
+
+    Field<weightedPosition> wps
+    (
+        mesh.nPoints(),
+        pTraits<weightedPosition>::zero
+    );
+
+    forAll(isMovingPoint, pointI)
+    {
+        if (isMovingPoint.test(pointI))
+        {
+            const labelList& pCells = mesh.pointCells(pointI);
+
+	    // First element of Tuple2 stores number of entries
+            wps[pointI].first() = pCells.size();
+
+	    // Second element of Tuple2 stores the sum of coordinates
+            for (const label celli : pCells)
+            {
+                wps[pointI].second() += mesh.cellCentres()[celli];
+            }
+        }
+    }
+
+    // Synchronize among processors
+    weightedPosition::syncPoints(mesh, wps);
+
+    // Calculate new point locations
+    tmp<pointField> tnewPoints(new pointField(mesh.nPoints(), Zero));
+    pointField& newPoints = tnewPoints.ref();
+
+    label nPoints = 0;
+    forAll(newPoints, pointI)
+    {
+        const weightedPosition& wp = wps[pointI];
+
+	// internal point
+        if (mag(wp.first()) > VSMALL)
+        {
+            newPoints[pointI] =
+                wp.second()/wp.first();
+            nPoints++;
+        }
+
+	// boundary point
+	else
+	{
+            newPoints[pointI] = mesh.points()[pointI];
+	}
+    }
+
+    Info << "Smoothed " << returnReduce(nPoints, sumOp<label>())
+         << " points." << endl;
+
+    return tnewPoints;
+}
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+    argList::addNote
+    (
+        "Move mesh points to increase mesh quality"
+    );
+    #include "addRegionOption.H"
+    #include "addOverwriteOption.H"
+
+    argList::addOption
+    (
+        "time",
+        "time",
+        "Specify the time (default is latest)"
+    );
+
+    #include "addOverwriteOption.H"
+    #include "setRootCase.H"
+    #include "createTime.H"
+    #include "createNamedMesh.H"
+
+    const bool overwrite = args.found("overwrite");
+    const word oldInstance = mesh.pointsInstance();
+
+    // Handle time
+    if (args.found("time"))
+    {
+        if (args["time"] == "constant")
+        {
+            runTime.setTime(instant(0, "constant"), 0);
+        }
+        else
+        {
+            const scalar timeValue = args.get<scalar>("time");
+            runTime.setTime(instant(timeValue), 0);
+        }
+    }
+
+    // Calculate new point locations and apply to mesh
+    tmp<pointField> newPoints = centroidalSmoothing(mesh);
+    mesh.movePoints(newPoints);
+
+    // Save mesh
+    {
+        if (!overwrite)
+        {
+            ++runTime;
+            mesh.setInstance(runTime.timeName());
+        }
+        else
+        {
+            mesh.setInstance(oldInstance);
+        }
+
+        // Set the precision of the points data to 10
+        IOstream::defaultPrecision(max(10u, IOstream::defaultPrecision()));
+
+        Info << "Writing new mesh to time " << runTime.timeName()
+             << nl << endl;
+
+        mesh.write();
+        runTime.printExecutionTime(Info);
+    }
+
+    Info << nl << "End" << nl << endl;
+
+    return 0;
+}
+
+// ************************************************************************* //
