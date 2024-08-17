0feb199e4d6be36d77b96bc93bdd6f05a60fec54
Tuomo Keskitalo <tuomo.keskitalo@iki.fi>
2024-08-17 16:12:31 +0300

    Add restrictAngleVarianceIncrease() constraint (WIP)
    - This constraint applies the variance of edge-edge angles of
      internal faces meeting at an internal point as a quality metric
      and constrains movement if variance would increase (quality
      would be decreased).
    - Currently commented out, as this seems to freeze some internal
      points, need to investigate further.

--- src/smoothMesh.C
+++ src/smoothMesh.C
@@ -19,6 +19,12 @@ Description
 #include "meshTools.H"
 #include <float.h>
 
+// #include <typeinfo>
+// Typeinfo is needed only for getting types while debugging, for example:
+// Info << "Type is " << typeid(x).name() << endl;
+// Use terminal command like this to demangle the mangled name:
+// c++filt -t N4Foam4faceE
+
 #include "orthogonalBoundaryBlending.C"
 
 using namespace Foam;
@@ -350,6 +356,183 @@ int constrainMaxStepLength
     return 0;
 }
 
+// Calculate the cosine of the edge angle of two connected edges.
+// The common shared point is cCoords argument. Edge end point
+// coordinates are p1Coords and p2Coords.
+double cosAlphaEdgeAngle
+(
+    const vector cCoords,
+    const vector p1Coords,
+    const vector p2Coords
+)
+{
+    vector vec1 = (p1Coords - cCoords);
+    vector vec2 = (p2Coords - cCoords);
+    vec1.normalise();
+    vec2.normalise();
+    const double cosAlpha = vec1 & vec2;
+    return cosAlpha;
+}
+
+// Find the neighbour point indices neighPI1 and neighPI2 for the
+// point with index pointI, where all points are part of face index
+// faceI.
+int getNeighbourPoints
+(
+    const fvMesh& mesh,
+    const label centerPointI,
+    const label faceI,
+    label *neighPI1,
+    label *neighPI2
+)
+{
+    int i = 0;
+    const face facePoints = mesh.faces()[faceI];
+    const label nPoints = facePoints.size();
+    // Info << "facePoints are " << facePoints << endl;
+    forAll(facePoints, pointI)
+    {
+        if (facePoints[pointI] == centerPointI)
+        {
+            // Handle index wrapping at beginning and at end
+            label prevI = pointI - 1;
+            if (pointI == 0)
+                prevI = nPoints - 1;
+
+            label nextI = pointI + 1;
+            if (pointI == nPoints - 1)
+                nextI = 0;
+
+            *neighPI1 = facePoints[prevI];
+            *neighPI2 = facePoints[nextI];
+            return 0;
+        }
+        i++;
+    }
+
+    FatalError << "Sanity broken, didn't find neighbour points for center point "
+               << centerPointI << " and face " << faceI << endl
+               << abort(FatalError);
+
+    return 0;
+}
+
+// Compute and return variance for argument vals (array of doubles).
+// Array length is n.
+double calcVariance
+(
+    double* vals,
+    label n
+)
+{
+    double mean_val = 0.0;
+    for (label i = 0; i < n; i++)
+    {
+        mean_val += vals[i];
+    }
+    mean_val /= static_cast<double>(n);
+
+    double variance = 0.0;
+    for (label i = 0; i < n; i++)
+    {
+        const double d = vals[i] - mean_val;
+        variance += d * d;
+    }
+    variance /= static_cast<double>(n);
+
+    return variance;
+}
+
+// Restrict the increase of variance of edge-edge angles of internal
+// faces. This is a mesh quality constraint aimed at avoiding
+// decrease of point quality during the smoothing process.
+// Returns normalized sum of final angle variances as a return value.
+double restrictAngleVarianceIncrease
+(
+    const fvMesh& mesh,
+    pointField& origPoints,
+    const bitSet isMovingPoint
+)
+{
+    double totVariance = 0.0;
+    int nMovingPoints = 0;
+
+    // Copy original points for temporary working point field
+    tmp<pointField> tNewPoints(new pointField(mesh.nPoints(), Zero));
+    pointField& newPoints = tNewPoints.ref();
+    forAll(newPoints, pointI)
+        newPoints[pointI] = origPoints[pointI];
+
+    forAll(origPoints, pointI)
+    {
+        if (! isMovingPoint.test(pointI))
+            continue;
+
+        nMovingPoints++;
+        const vector cCoords = mesh.points()[pointI];
+        vector nCoords = origPoints[pointI];
+
+        // Calculate edge-edge cosine angles and their variance for
+        // all faces meeting at the current point coordinates
+        const label nPointFaces = mesh.pointFaces()[pointI].size();
+        // Info << "Processing point " << pointI << " which has "
+        //      << nPointFaces << " faces" << endl;
+        double cosAngles0[nPointFaces];
+        forAll(mesh.pointFaces()[pointI], pointFI)
+        {
+            // For each edge connected at this point, find the other point
+            label neighPI1 = 0;
+            label neighPI2 = 0;
+            const label faceI = mesh.pointFaces()[pointI][pointFI];
+            getNeighbourPoints(mesh, pointI, faceI, &neighPI1, &neighPI2);
+            const double cosAlpha = cosAlphaEdgeAngle(cCoords, origPoints[neighPI1], origPoints[neighPI2]);
+            cosAngles0[pointFI] = cosAlpha;
+            // Info << "Indices " << pointI << ":" << neighPI1 << "-"
+            //    << neighPI2 << " - " << cosAlpha << endl;
+        }
+        const double variance0 = calcVariance(cosAngles0, nPointFaces);
+
+        // Calculate edge-edge cosine angles and variance for the
+        // proposed new point coordinates
+        double cosAngles1[nPointFaces];
+        forAll(mesh.pointFaces()[pointI], pointFI)
+        {
+            // For each edge connected at this point, find the other point
+            label neighPI1 = 0;
+            label neighPI2 = 0;
+            const label faceI = mesh.pointFaces()[pointI][pointFI];
+            getNeighbourPoints(mesh, pointI, faceI, &neighPI1, &neighPI2);
+            const double cosAlpha = cosAlphaEdgeAngle(nCoords, origPoints[neighPI1], origPoints[neighPI2]);
+            cosAngles1[pointFI] = cosAlpha;
+        }
+        const double variance1 = calcVariance(cosAngles1, nPointFaces);
+
+        // If variance increases when moving point to new coordinates,
+        // then freeze the point to current coordinates, othewise leave
+        // the new point coordinates unchanged.
+        if (variance1 > variance0)
+        {
+            nCoords = cCoords;
+            totVariance += variance0;
+        }
+        else
+        {
+            totVariance += variance1;
+        }
+
+        // Save the constrained point
+        newPoints[pointI] = nCoords;
+    }
+
+    // Save new point coordinates to original point field
+    forAll(origPoints, pointI)
+    {
+        origPoints[pointI] = newPoints[pointI];
+    }
+
+    return (totVariance / static_cast<double>(nMovingPoints));
+}
+
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
@@ -482,11 +665,15 @@ int main(int argc, char *argv[])
         // Constrain absolute length of jump to new coordinates, to stabilize smoothing
         constrainMaxStepLength(mesh, newPoints, isInternalPoint, maxStepLength);
 
+        // WIP: Avoid increase of angle variance (a simple point quality metric)
+        // const double totVariance = restrictAngleVarianceIncrease(mesh, newPoints, isInternalPoint);
+        // Info << "Total normalized angle variance = " << totVariance << endl;
+
         // Old step length control disabled: Constrain the local step
         // length by fraction of shortest edge length
         // constrainLocalStepLength(mesh, newPoints, isInternalPoint, 0.05);
 
-        // Avoid shortening of edge length
+        // Avoid shortening of short edge length
         restrictEdgeShortening(mesh, newPoints, isInternalPoint, minEdgeLength, maxEdgeLength);
 
         mesh.movePoints(tNewPoints);
