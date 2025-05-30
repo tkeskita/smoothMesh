31c440bb1e61beff08af2b4d30c76f77566c8131
Tuomo Keskitalo <tuomo.keskitalo@iki.fi>
2025-05-30 15:23:34 +0300

    Added testcase2
    - This test case is the sphere void in box, which illustrates
      the issue in #7.
    - Added testcase2 to run_tests.sh.

--- run_tests.sh
+++ run_tests.sh
@@ -8,21 +8,31 @@ if [ $retVal -ne 0 ]; then
     exit 1
 fi
 
-# Clean up test folder
+# Clean up test folder and initialize
 test_folder="./run_tests"
 echo "Removing $test_folder"
 rm -rf $test_folder
 echo "Populating $test_folder"
 mkdir $test_folder
 cp -rv testcase* $test_folder &> /dev/null
+cd $test_folder
 
-cd $test_folder/testcase
-folder=`pwd`
-echo "Running run_parallel at $folder"
-./run_parallel | grep Smoothing\ iteration
-echo "Running run_serial at $folder"
-./run_serial | grep Smoothing\ iteration
-cd ..
+# Function for the tests
+function do_test() {
+  cd $1
+  folder=`pwd`
+  echo "Running run_parallel at $folder"
+  ./run_parallel | grep Smoothing\ iteration
+  mpirun -np 3 checkMesh -parallel -latestTime | grep \<\<
+  echo "Running run_serial at $folder"
+  ./run_serial | grep Smoothing\ iteration
+  checkMesh -latestTime | grep \<\<
+  cd ..
+}
+
+# Tests
+do_test "testcase"
+do_test "testcase2"
 
 echo "Test runs completed"
 cd ..
