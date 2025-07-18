d4f5bb5c9c3e34de2c0ae88a0ae6d933a89c68a0
Tuomo Keskitalo <tuomo.keskitalo@iki.fi>
2025-07-18 16:52:09 +0300

    Add function findClosestPointI()
    - This function finds the index of closest point of a face in
      triSurface.

--- src/boundaryPointSmoothing.C
+++ src/boundaryPointSmoothing.C
@@ -18,6 +18,35 @@ using namespace Foam;
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
+// Find index of point in triSurface mesh which is part of a given
+// face index faceI and is located nearest to coordinates coords
+
+label findClosestPointI
+(
+    const triSurface& surf,
+    const label faceI,
+    const point& coords
+)
+{
+    scalar distance = VGREAT;
+    label closestPointI = -1;
+
+    forAll(surf.localFaces()[faceI], pointI)
+    {
+        const label localPointI = surf.localFaces()[faceI][pointI];
+        const point& v = surf.localPoints()[localPointI];
+        const scalar testDistance = mag(coords - v);
+        if (testDistance < distance)
+        {
+            closestPointI = localPointI;
+            distance = testDistance;
+        }
+    }
+
+    return closestPointI;
+}
+
+
 // Projection of boundary points to feature edges and boundary surfaces
 
 int projectBoundaryPointsToEdgesAndSurfaces
@@ -48,8 +77,8 @@ int projectBoundaryPointsToEdgesAndSurfaces
                 << exit(FatalError);
         }
 
-        // Closest face info
-        // const point& closestPoint = hitInfo.hitPoint();
+        // Closest point and face info
+        const point& closestPoint = hitInfo.hitPoint();
         const label faceI = hitInfo.index();
         const point fCoords = surf.faceCentres()[faceI];
         const vector faceNormal =
@@ -60,6 +89,12 @@ int projectBoundaryPointsToEdgesAndSurfaces
         const vector cf = cCoords - fCoords;
         const double dotProd = cf & faceNormal;
         const vector pCoords = cCoords - dotProd * faceNormal;
+        // Info << "  - projected point coords " << pCoords << endl;
+        // Info << "    closest point coords   " << closestPoint << endl;
+
+        // Search closest point index
+        const label closestPointI = findClosestPointI(surf, faceI, closestPoint);
+        Info << "Closest pointI is " << closestPointI << " at " << surf.localPoints()[closestPointI] << endl;
 
         // Info << "Hit point for pointI " << pointI << " at " << pt << " is " << closestPoint << " with face index " << hitInfo.index() << " and face center " << faceCenter << endl;
 
