6f5d26be553bf9eae80f0461d3be460fa4aeb35c
Tuomo Keskitalo <tuomo.keskitalo@iki.fi>
2025-12-22 17:12:13 +0200

    Added addEdgePointsToIsland()
    - This function finds the edge points and adds them to specified
      island.
    - Added also function isPointInIsland() to check whether a point
      is part of a given island.

--- src/layerTreatment.C
+++ src/layerTreatment.C
@@ -59,11 +59,29 @@ label findNewPrismBoundaryPointI
 }
 
 
+// Help function to test whether a point belongs to a given island
+
+bool isPointInIsland
+(
+    const label pointI,
+    const label islandI,
+    const labelList& prismIslands1,
+    const labelList& prismIslands2,
+    const labelList& prismIslands3
+)
+{
+    return ((prismIslands1[pointI] == islandI) or
+        (prismIslands2[pointI] == islandI) or
+        (prismIslands3[pointI] == islandI)
+    );
+}
+
+
 // Help function to set a given island info for a point index. If no
 // island slots are available, reset all slots to ignored value and
-// return false to indicate that point has been reset.
+// return zero to indicate that point has been reset.
 
-bool addIslandInfoForPoint
+label addIslandInfoForPoint
 (
     const label pointI,
     const label pointNormalSourceI,
@@ -83,13 +101,18 @@ bool addIslandInfoForPoint
     vectorList& pointNormals3
 )
 {
+    if (isPointInIsland(pointI, islandI, prismIslands1, prismIslands2, prismIslands3))
+    {
+        FatalError << "Point " << pointI << " is already in island " << islandI << endl << abort(FatalError);
+    }
+
     if (prismIslands1[pointI] == UNDEF_LABEL)
     {
         prismIslands1[pointI] = islandI;
         pointNormalSource1[pointI] = pointNormalSourceI;
         pointNormals1[pointI] = pointNormals[pointNormalSourceI];
         pointHops1[pointI] = 0;
-        return true;
+        return 1;
     }
     else if (prismIslands2[pointI] == UNDEF_LABEL)
     {
@@ -97,7 +120,7 @@ bool addIslandInfoForPoint
         pointNormalSource2[pointI] = pointNormalSourceI;
         pointNormals2[pointI] = pointNormals[pointNormalSourceI];
         pointHops2[pointI] = 0;
-        return true;
+        return 2;
     }
     else if (prismIslands3[pointI] == UNDEF_LABEL)
     {
@@ -105,7 +128,7 @@ bool addIslandInfoForPoint
         pointNormalSource3[pointI] = pointNormalSourceI;
         pointNormals3[pointI] = pointNormals[pointNormalSourceI];
         pointHops3[pointI] = 0;
-        return true;
+        return 3;
     }
 
     // All slots are already in use, reset slots to ignored
@@ -121,7 +144,7 @@ bool addIslandInfoForPoint
     pointHops1[pointI] = UNDEF_LABEL;
     pointHops2[pointI] = UNDEF_LABEL;
     pointHops3[pointI] = UNDEF_LABEL;
-    return false;
+    return 0;
 }
 
 
@@ -197,6 +220,109 @@ label propagateIslandInfoOnBoundary
 }
 
 
+// Help function to add non-prismatic edge points to the island
+
+label addEdgePointsToIsland
+(
+    const fvMesh& mesh,
+    const label islandI,
+    const boolList& isPrismaticPoint,
+    const boolList& isLayerSurfacePoint,
+    const vectorList& pointNormals,
+    labelList& prismIslands1,
+    labelList& prismIslands2,
+    labelList& prismIslands3,
+    labelList& pointHops1,
+    labelList& pointHops2,
+    labelList& pointHops3,
+    labelList& pointNormalSource1,
+    labelList& pointNormalSource2,
+    labelList& pointNormalSource3,
+    vectorList& pointNormals1,
+    vectorList& pointNormals2,
+    vectorList& pointNormals3
+)
+{
+    labelList edgePoints;
+
+    // Identify edge points
+    forAll(mesh.boundary(), patchI)
+    {
+        const label startI = mesh.boundary()[patchI].start();
+        const label endI = startI + mesh.boundary()[patchI].Cf().size();
+
+        for (label faceI = startI; faceI < endI; faceI++)
+        {
+            const face& f = mesh.faces()[faceI];
+
+            // Is this face part of island?
+            bool isIslandFace = false;
+            forAll (f, facePointI)
+            {
+                const label pointI = mesh.faces()[faceI][facePointI];
+                if (isPointInIsland(pointI, islandI, prismIslands1, prismIslands2, prismIslands3))
+                {
+                    isIslandFace = true;
+                    break;
+                }
+            }
+
+            // Add non-island face points to edge points
+            if (isIslandFace)
+            {
+                forAll (f, facePointI)
+                {
+                    const label pointI = mesh.faces()[faceI][facePointI];
+                    if (! isPointInIsland(pointI, islandI, prismIslands1, prismIslands2, prismIslands3))
+                    {
+                        if (findIndex(edgePoints, pointI) == -1)
+                        {
+                            edgePoints.append(pointI);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    // Propagate point normal source to edge points
+    label n = 1;
+    label nTot = 0;
+
+    while (n > 0)
+    {
+        n = 0;
+
+        forAll(edgePoints, edgePointI)
+        {
+            const label pointI = edgePoints[edgePointI];
+
+            if (! isLayerSurfacePoint[pointI])
+                continue;
+            if (isPointInIsland(pointI, islandI, prismIslands1, prismIslands2, prismIslands3))
+                continue;
+
+            forAll (mesh.pointPoints()[pointI], pointPointI)
+            {
+                const label neighI = mesh.pointPoints()[pointI][pointPointI];
+                if (! isLayerSurfacePoint[neighI])
+                    continue;
+
+                if (isPointInIsland(neighI, islandI, prismIslands1, prismIslands2, prismIslands3))
+                {
+                    addIslandInfoForPoint(pointI, pointNormalSource1[neighI], islandI, pointNormals, prismIslands1, prismIslands2, prismIslands3, pointHops1, pointHops2, pointHops3, pointNormalSource1, pointNormalSource2, pointNormalSource3, pointNormals1, pointNormals2, pointNormals3);
+                    ++n;
+                    ++nTot;
+                    // Pout << "  Added edge point " << pointI << " to island " << islandI << " res " << res << endl;
+                    break;
+                }
+            }
+        }
+    }
+
+    return nTot;
+}
+
 // Function to identify and number the prismatic islands on the
 // boundaries
 
@@ -248,10 +374,10 @@ int identifyPrismaticBoundaryIslands
 
         const label n = propagateIslandInfoOnBoundary(mesh, startPointI, isVisitedPoint, isPrismaticPoint, isLayerSurfacePoint, pointNormals, prismIslands1, prismIslands2, prismIslands3, pointHops1, pointHops2, pointHops3, pointNormalSource1, pointNormalSource2, pointNormalSource3, pointNormals1, pointNormals2, pointNormals3);
 
-        Pout << "Island " << islandI << " has " << n << " prism points" << endl;
-        
         // Add the non-prismatic edge points to this island
-        // addEdgePointsToPrismIndex(mesh, islandI, isPrismaticPoint, isLayerSurfacePoint, prismIslands1, prismIslands2, prismIslands3);
+        const label ne = addEdgePointsToIsland(mesh, islandI, isPrismaticPoint, isLayerSurfacePoint, pointNormals, prismIslands1, prismIslands2, prismIslands3, pointHops1, pointHops2, pointHops3, pointNormalSource1, pointNormalSource2, pointNormalSource3, pointNormals1, pointNormals2, pointNormals3);
+
+        Pout << "Island " << islandI << " has " << n << " prism points" << " and " << ne << " edge points" << endl;
 
         if (n < 1)
         {
